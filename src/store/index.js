6d1f3efcaba0000b0c57ee58f7f561c7e98b1d76
Hung Jin Chong <hungjinchong@Outlook.com>
2021-03-03 16:34:10 +0000

    can create conversations now

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import errorsReducer from "../reducers/errors";
+import conversationsReducer from "../reducers/conversations";
+import messagesReducer from "../reducers/messages";
+import selectedConvoReducer from "../reducers/selectedConvo";
 import thunk from "redux-thunk";
 
 const composedEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -11,10 +14,14 @@ const initialState = {
   posts: [],
   conversations: [],
   messages: [],
+  selectedConvo: null,
 };
 
 const allReducers = combineReducers({
   errors: errorsReducer,
+  conversations: conversationsReducer,
+  messages: messagesReducer,
+  selectedConvo: selectedConvoReducer,
 });
 
 export default function configureStore() {
